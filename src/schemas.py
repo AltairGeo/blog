b77c881e92cc457a64a226dcf9e6f3c76bbfdc0c
Altair <altairgeo000@gmail.com>
2025-01-06 03:11:10 +0300

    micro fixes

--- src/schemas.py
+++ src/schemas.py
@@ -34,9 +34,11 @@ class Post(BasePost):
     author_id: int
 
 
-class CreatePost(BasePost):
+class CreatePost(BaseModel):
+    title: str
+    text: str
+    created_at: datetime = datetime.now()
     token: Token
-
 
 class AvatarHashGenerate(BaseModel):
     id: int
