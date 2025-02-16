ab6edfbac3fe33e4181d0e42eef2279e374b7ee4
Altair <altairgeo000@gmail.com>
2025-02-16 23:43:29 +0300

    core fix

--- src/db/core.py
+++ src/db/core.py
@@ -2,7 +2,14 @@ from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine, Asyn
 from sqlalchemy.orm import DeclarativeBase
 from settings import AppSettings
 
-engine = create_async_engine(AppSettings.db_url,pool_size=20, max_overflow=0, pool_recycle=300, pool_pre_ping=True ) # Create engine
+try:
+    engine = create_async_engine(AppSettings.db_url,pool_size=20, max_overflow=0, pool_recycle=300, pool_pre_ping=True ) # Create engine
+except:
+    try:
+        engine = create_async_engine(AppSettings.db_url)
+    except Exception as e:
+        print(e)
+        raise e
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False)  # Create session maker
 
 class ModelBase(DeclarativeBase, AsyncAttrs):
