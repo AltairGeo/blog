f32c7b0b78039faf12efaa8f291058ec715b13e0
AltairGeo <altairgeo000@gmail.com>
2025-03-12 18:23:12 +0300

    logging

--- src/db/core.py
+++ src/db/core.py
@@ -8,11 +8,12 @@ from settings import AppSettings
 try:
     engine = create_async_engine(AppSettings.db_url, pool_size=20, max_overflow=0, pool_recycle=300,
                                  pool_pre_ping=True)  # Create engine
-except:
+except Exception as e:
+    logging.warning("Error with create db connection with production settings.")
     try:
         engine = create_async_engine(AppSettings.db_url)
     except Exception as e:
-        logging.error(str(e))
+        logging.error(f"Error with creation db engine: {e}")
         raise e
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False)  # Create session maker
 
